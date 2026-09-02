4ba7e9cd4b4a9ef3daa5b71ce8f29dff933e9a92
Cayky153 <antonio.cayky987@gmail.com>
2026-09-02 03:53:25 -0300

    feat:routes funcional

--- backend/src/modules/projetos/routes.js
+++ backend/src/modules/projetos/routes.js
@@ -1,7 +1,9 @@
 import express from 'express';
-
+//mesma motivação da importação do repository
+import * as controller from "./controller.js"
+import validarProjeto from "./validation.js" 
 
 const router = express.Router();
-router.post('/adicionar', controller);
+router.post('/adicionar', validarProjeto, controller.adicionarProjeto);
 
 export default router;
