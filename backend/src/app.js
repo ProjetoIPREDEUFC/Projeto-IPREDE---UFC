3671fc0b35553bd0e6fa0fb90430004720714e77
Cayky153 <antonio.cayky987@gmail.com>
2026-09-02 03:55:00 -0300

    feat: app agora utiliza o middleware de tratamento de erro

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,7 +1,10 @@
 import express from 'express'
-
+import errorHandler from './middlewares/errorHandler.js';
+import rotasProjetos from './modules/projetos/routes.js'
 const app = express();
 
 app.use(express.json());
+app.use('/projetos',rotasProjetos);
+app.use(errorHandler);
 
 export default app
